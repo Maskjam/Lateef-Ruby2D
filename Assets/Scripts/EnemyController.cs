e53c3ef207cd1a7df6f035baf0b8ccf6de651b8b
Maskjam <113995112+Maskjam@users.noreply.github.com>
2023-01-23 09:11:50 -0500

    done

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -15,6 +15,8 @@ public class EnemyController : MonoBehaviour
     Animator animator;
     bool broken = true;
 
+    public ParticleSystem smokeEffect;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -76,5 +78,7 @@ public class EnemyController : MonoBehaviour
         broken = false;
         rigidbody2D.simulated = false;
         animator.SetTrigger("Fixed");
+
+       smokeEffect.Stop();
     }
 }
